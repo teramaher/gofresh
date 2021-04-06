173cffced80a1ad685baad66e4cdadef4f71e76c
teramaher <59835785+teramaher@users.noreply.github.com>
2021-04-05 19:16:29 -0500

    add code

---
+++ shop/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+            return reverse('shop:product_list_by_category',
+                           args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+            return reverse('shop:product_detail',
+                           args=[self.id, self.slug])
+
+
+
+class User(models.Model):
+    user_first_name = models.CharField(max_length=50)
+    user_last_name = models.CharField(max_length=50)
+    user_email = models.EmailField(max_length=100)
+    user_phone_number = models.CharField(max_length=50)
+    user_status = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.user_first_name)
